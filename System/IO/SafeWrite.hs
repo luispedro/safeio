0edeb2357ff4f46e76d4255b42903aebd437bd13
Luis Pedro Coelho <luis@luispedro.org>
2023-06-26 16:25:18 +0200

    ENH Make code compatible with unix-2.8
    closes #2

--- System/IO/SafeWrite.hs
+++ System/IO/SafeWrite.hs
@@ -12,8 +12,18 @@ import           System.IO (Handle, hClose, openTempFile)
 import           System.Directory (renameFile, removeFile)
 
 #ifndef WINDOWS
-import           System.Posix.IO (openFd, defaultFileFlags, closeFd, OpenMode(..))
+import           System.Posix.IO (defaultFileFlags, closeFd, OpenMode(..))
+import qualified System.Posix.IO
+import qualified System.Posix.Types
 import           System.Posix.Unistd (fileSynchronise)
+#endif
+
+
+openFd :: FilePath -> OpenMode -> System.Posix.IO.OpenFileFlags -> IO System.Posix.Types.Fd
+#if MIN_VERSION_base (4,18,0)
+openFd = System.Posix.IO.openFd
+#else
+openFd fname mode flags = System.Posix.IO.openFd fname mode Nothing flags
 #endif
 
 -- | Sync a file to disk
@@ -23,11 +33,11 @@ syncFile :: FilePath -- ^ File to sync
             -> IO ()
 #ifndef WINDOWS
 syncFile fname = do
-    bracket (openFd fname ReadWrite Nothing defaultFileFlags)
+    bracket (openFd fname ReadWrite defaultFileFlags)
         closeFd
         fileSynchronise
     -- The code below will not work on Windows
-    bracket (openFd (takeDirectory fname) ReadOnly Nothing defaultFileFlags)
+    bracket (openFd (takeDirectory fname) ReadOnly defaultFileFlags)
         closeFd
         fileSynchronise
 #else
